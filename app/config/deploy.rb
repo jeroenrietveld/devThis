3a901cd2d2e05834673f13816d1bad5d60e98d94
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-03-27 13:33:33 +0100

    deployment

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,20 +1,32 @@
 set :application, "devThis"
 set :domain,      "dev-this.com"
-set :deploy_to,   "/home/wesley/domains/#{domain}"
+set :deploy_to,   "/home/wesley/domains/#{domain}/public_html"
 set :app_path,    "app"
+set :use_sudo,    false
 
-set :repository,  "git@github.com:hoppinger/koersvo-dgm.git"
+set :repository,  "git@github.com:jeroenrietveld/devThis.git"
 set :scm,         :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+set :branch,      "master"
+
+set :writable_dirs,   ["app/cache", "app/logs",]
+
+set :keep_releases,    3
+set :update_vendors,   false
+set :interactive_mode, false
 
 set :model_manager, "doctrine"
 # Or: `propel`
+
+default_run_options[:pty] = true
+set :ssh_options, { :forward_agent => true }
+ssh_options[:keys] = %w('~/.ssh/id_rsa.pub')
+ssh_options[:port] = "5678"
+set :user,           "root"
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
-set  :keep_releases,  3
-
 # Be more verbose by uncommenting the following line
-# logger.level = Logger::MAX_LEVEL
+logger.level = Logger::MAX_LEVEL
