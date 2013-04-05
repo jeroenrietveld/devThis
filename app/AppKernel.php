5e7835823bb043570022f1b6288522e655dd89a5
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-04-05 17:07:28 +0200

    added twitter and mopa bootstrap and a custom theme

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new DevThis\DefaultBundle\DevThisDefaultBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
