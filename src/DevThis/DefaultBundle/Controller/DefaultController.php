31b9ab346dc77ba85fffcb437bcb2d6a0d33edac
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-04-08 09:42:07 +0200

    refactored ugly menu rendering, added some routes to category views

--- src/DevThis/DefaultBundle/Controller/DefaultController.php
+++ src/DevThis/DefaultBundle/Controller/DefaultController.php
@@ -4,18 +4,39 @@ namespace DevThis\DefaultBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use DevThis\DefaultBundle\Entity\Catergory,
+use DevThis\DefaultBundle\Entity\Category,
 	DevThis\DefaultBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        return $this->render('DevThisDefaultBundle:Default:index.html.twig');
+    }
+
+    public function menuRenderAction()
+    {
     	$em 		= $this->getDoctrine()->getManager();
     	$categories = $em->getRepository('DevThisDefaultBundle:Category')->findAll();
 
-        return $this->render('DevThisDefaultBundle:Default:index.html.twig', array(
+        return $this->render('DevThisDefaultBundle:Default:menu.html.twig', array(
         	'categories' => $categories
         ));
+    }
+
+    public function categoryAction($category)
+    {
+    	$em = $this->getDoctrine()->getManager();
+    	$category = $em->getRepository('DevThisDefaultBundle:Category')->find($category);
+    	$categories = $em->getRepository('DevThisDefaultBundle:Category')->findAll();
+
+    	if(!$category instanceof Category) {
+    		throw $this->createNotFoundException('Could not find category.');
+    	}
+
+    	return $this->render('DevThisDefaultBundle:Default:category.html.twig', array(
+    		'category' => $category,
+        	'categories' => $categories
+    	));
     }
 }
